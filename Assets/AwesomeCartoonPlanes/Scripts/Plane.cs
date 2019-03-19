eb332d5a131f5941b1ded3f535827eca66226d85
Bob Pikaar <bobpikaar@gmail.com>
2019-03-19 15:18:22 +0100

    Initial commit

---
+++ Assets/AwesomeCartoonPlanes/Scripts/Plane.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Plane : MonoBehaviour {
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private GameObject _projectile;
+	public GameObject prop;
+	public GameObject propBlured;
+
+    [SerializeField]
+	private bool _engineOn;
+
+    [SerializeField]
+    private Transform _shotSpawn;
+    [SerializeField]
+    private float _fireRate;
+    private float nextFire;
+
+    [SerializeField]
+    private float _turnSpeed;
+
+    void Update () 
+	{
+		if (_engineOn) {
+			prop.SetActive (false);
+			propBlured.SetActive (true);
+			propBlured.transform.Rotate (1000 * Time.deltaTime, 0, 0);
+		} else {
+			prop.SetActive (true);
+			propBlured.SetActive (false);
+		}
+
+        if (Input.GetButton("Jump") && Time.time > nextFire)
+        {
+            nextFire = Time.time + _fireRate;
+            Instantiate(_projectile, _shotSpawn.position, _shotSpawn.rotation);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal    = Input.GetAxis("Horizontal");
+        float moveVertical      = Input.GetAxis("Vertical");
+
+        if (_engineOn)
+        {
+            GetComponent<Rigidbody>().velocity = transform.up * -_speed;
+
+            transform.Rotate(
+                new Vector3(
+                    moveVertical * _turnSpeed,
+                    moveHorizontal * _turnSpeed,
+                    moveHorizontal * 0.3f * _turnSpeed),
+                Space.Self);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject rocketClone = Instantiate(
+            _projectile,
+            transform.position,
+            new Quaternion(90, 0, 0, 0)) as GameObject;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Unity-3D-Planes
-A Unity 3D project with firing airplanes 
+A Unity 3D project with firing airplanes.
+
+In this lesson we'll take our first look at the Unity editor. We will learn how to use the interface, use 3D objects, and then we'll make a basic game with interaction.
+
+## Installation 
+- Install [Unity](https://unity3d.com/get-unity/download)
+- Install a code editor for c# (Visual Studio or Monodevelop)
+- clone this repo 
+
+
